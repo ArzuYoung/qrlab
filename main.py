711b8f100f7f9f66b8903024f313acbfe996dba8
Arzu Rzaeva <AMRzaeva@alfabank.ru>
2023-11-23 01:29:52 +0500

    add barcode

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from pyzbar import pyzbar
 import cv2
 import numpy as np
 import os
+import barcode
+from barcode.writer import ImageWriter
 
 filename = 'qr.png'
+bar_filename = 'barcode'
+qr_filename = 'qr'
+bar_filename_full = 'barcode.png'
 path = '/Users/alfa/PycharmProjects/qrlab/'
 answers = {0: "неуспешно декодировано", 1: "успешно декодировано"}
-# example_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+example_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+example_barcode = "123456789012"
 
 
 def make_dir(dir_name):
@@ -15,6 +21,18 @@ def make_dir(dir_name):
         os.mkdir(path + dir_name)
     except OSError:
         print("directory " + dir_name + " exists")
+
+
+def make_bar_code(value):
+    try:
+        int(value)
+    except OSError:
+        print("Штрих код состоит только из цифр")
+
+    ean = barcode.get_barcode_class('ean13')
+    ean2 = ean(value, writer=ImageWriter())
+    print(ean2)
+    ean2.save(bar_filename)
 
 
 def make_qr_code(value):
@@ -26,32 +44,32 @@ def make_qr_code(value):
     qr_img.save(filename)
 
 
-def decode_qr(qr_img):
+def decode_code(qr_img):
     codes = pyzbar.decode(qr_img)
     if len(codes) == 0:
         return None
     return codes[0].data.decode('utf-8')
 
 
-def brightness_increase(name, context, step=1, gamma=15):
-    dir_name = 'brightness'
+def brightness_increase(name, context, code_type, step=1, gamma=15):
+    dir_name = code_type + '/' + 'brightness'
     make_dir(dir_name)
     img = cv2.imread(name)
     br_img = cv2.addWeighted(img, 1, np.zeros_like(img), 0, gamma)
     while True:
-        if decode_qr(br_img) == context:
+        if decode_code(br_img) == context:
             gamma += step
             br_img = cv2.addWeighted(img, 1, np.zeros_like(img), 0, gamma)
         else:
             break
 
-    cv2.imwrite(os.path.join(path, dir_name + '/none_' + filename), br_img)
-    cv2.imwrite(os.path.join(path, dir_name + '/done_' + filename),
+    cv2.imwrite(os.path.join(path, dir_name + '/none_' + name), br_img)
+    cv2.imwrite(os.path.join(path, dir_name + '/done_' + name),
                 cv2.addWeighted(img, 1, np.zeros_like(img), 0, gamma-step))
 
 
-def rotate(name, context, step=5):
-    dir_name = 'rotate'
+def rotate(name, context, code_type, step=5):
+    dir_name = code_type + '/' + 'rotate'
     make_dir(dir_name)
     img = cv2.imread(name)
     (h, w) = img.shape[:2]
@@ -61,48 +79,49 @@ def rotate(name, context, step=5):
     for angle in range(0, 360, step):
         M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
         rotated = cv2.warpAffine(img, M, (w, h))
-        if decode_qr(rotated) != context:
-            cv2.imwrite(os.path.join(path, dir_name + "/none_" + str(angle) + "_" + filename), rotated)
+        if decode_code(rotated) != context:
+            cv2.imwrite(os.path.join(path, dir_name + "/none_" + str(angle) + "_" + name), rotated)
             none_angles.append(angle)
         else:
-            cv2.imwrite(os.path.join(path, dir_name + "/done_" + str(angle) + "_" + filename), rotated)
+            cv2.imwrite(os.path.join(path, dir_name + "/done_" + str(angle) + "_" + name), rotated)
             done_angles.append(angle)
     print("Углы с успешным декодированием:", ', '.join(map(str, done_angles)))
     print("Углы с неудачным декодированием:", ', '.join(map(str, none_angles)))
 
 
-def flip(name, context):
-    dir_name = 'flip'
+def flip(name, context, code_type):
+    dir_name = code_type + '/' + 'flip'
     make_dir(dir_name)
     img = cv2.imread(name)
     horizontal_flip_img = cv2.flip(img, 1)
     vertical_flip_img = cv2.flip(img, 0)
-    cv2.imwrite(os.path.join(path, dir_name + "/horizontal_flip_" + filename), horizontal_flip_img)
-    cv2.imwrite(os.path.join(path, dir_name + "/vertical_flip" + filename), vertical_flip_img)
+    cv2.imwrite(os.path.join(path, dir_name + "/horizontal_flip_" + name), horizontal_flip_img)
+    cv2.imwrite(os.path.join(path, dir_name + "/vertical_flip" + name), vertical_flip_img)
 
-    print("Отражение по горизонтали - ", answers[decode_qr(horizontal_flip_img) == context])
-    print("Отражение по вертикали - ", answers[decode_qr(vertical_flip_img) == context])
+    print("Отражение по горизонтали - ", answers[decode_code(horizontal_flip_img) == context])
+    print("Отражение по вертикали - ", answers[decode_code(vertical_flip_img) == context])
 
 
-def blur(name, context, step=1, core_item=1):
-    dir_name = 'blur'
+def blur(name, context, code_type, step=1, core_item=1):
+    dir_name = code_type + '/' + 'blur'
     make_dir(dir_name)
     img = cv2.imread(name)
     blur_img = cv2.blur(img, (core_item, core_item))
     while True:
-        if decode_qr(blur_img) == context:
+        d = decode_code(blur_img)
+        if d == context:
             core_item += step
             blur_img = cv2.blur(img, (core_item, core_item))
         else:
             break
 
-    cv2.imwrite(os.path.join(path, dir_name + '/none_' + filename), blur_img)
-    cv2.imwrite(os.path.join(path, dir_name + '/done_' + filename),
+    cv2.imwrite(os.path.join(path, dir_name + '/none_' + name), blur_img)
+    cv2.imwrite(os.path.join(path, dir_name + '/done_' + name),
                 cv2.blur(img, (core_item - step, core_item - step)))
 
 
-def center_crop(name, context, step=5):
-    dir_name = 'center_crop'
+def center_crop(name, context, code_type, step=5):
+    dir_name = code_type + '/' + 'center_crop'
     make_dir(dir_name)
     img = cv2.imread(name)
     (h, w) = img.shape[:2]
@@ -110,22 +129,22 @@ def center_crop(name, context, step=5):
     (cX, cY) = (w // 2, h // 2)
     while l > 0:
         cropped_img = img[cX - l:cX + l, cY - l:cY + l]
-        if decode_qr(cropped_img) != context:
-            cv2.imwrite(os.path.join(path, dir_name + '/none_' + filename), cropped_img)
-            cv2.imwrite(os.path.join(path, dir_name + '/done_' + filename),
+        if decode_code(cropped_img) != context:
+            cv2.imwrite(os.path.join(path, dir_name + '/none_' + name), cropped_img)
+            cv2.imwrite(os.path.join(path, dir_name + '/done_' + name),
                         img[cX - (l + step):cX + (l + step), cY - (l + step):cY + (l + step)])
             break
         l -= step
 
 
-def side_crop(name, context, angle, step=5):
+def side_crop(name, context, code_type, angle, step=5):
     side_dict = {
         0: "left_up_",
         90: "right_up_",
         180: "right_down_",
         270: "left_down_"
     }
-    dir_name = 'side_crop'
+    dir_name = code_type + '/' + 'side_crop'
     make_dir(dir_name)
     img = cv2.imread(name)
     (h, w) = img.shape[:2]
@@ -135,31 +154,40 @@ def side_crop(name, context, angle, step=5):
     rotated_img = cv2.warpAffine(img, M, (w, h))
     while l > 0:
         cropped_img = rotated_img[:l, :l]
-        if decode_qr(cropped_img) != context:
+        if decode_code(cropped_img) != context:
             M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-            cv2.imwrite(os.path.join(path, dir_name + '/none_' + side_dict[angle] + filename),
+            cv2.imwrite(os.path.join(path, dir_name + '/none_' + side_dict[angle] + name),
                         cv2.warpAffine(cropped_img, M, (w, h)))
-            cv2.imwrite(os.path.join(path, dir_name + '/done_' + side_dict[angle] + filename),
+            cv2.imwrite(os.path.join(path, dir_name + '/done_' + side_dict[angle] + name),
                         cv2.warpAffine(rotated_img[:l + 1, :l + 1], M, (w, h)))
             break
         l -= step
 
 
-def make_augmentations(filename, context):
-    brightness_increase(filename, context)
-    rotate(filename, context)
-    flip(filename, context)
-    blur(filename, context)
-    center_crop(filename, context)
-    side_crop(filename, context, 0)
-    side_crop(filename, context, 90)
-    side_crop(filename, context, 180)
-    side_crop(filename, context, 270)
+def make_augmentations(filename, context, code_type):
+    make_dir(code_type)
+    brightness_increase(filename, context, code_type)
+    rotate(filename, context, code_type)
+    flip(filename, context, code_type)
+    blur(filename, context, code_type)
+    center_crop(filename, context, code_type)
+    side_crop(filename, context, code_type, 0)
+    side_crop(filename, context, code_type, 90)
+    side_crop(filename, context, code_type, 180)
+    side_crop(filename, context, code_type, 270)
 
 
 if __name__ == '__main__':
     context = input("Введите данные для формирования qr-кода: ")
+    if context == "":
+        context = example_url
     make_qr_code(context)
-    decode_context = decode_qr(cv2.imread(filename))
+    decode_context = decode_code(cv2.imread(filename))
     print("Декодированный код: ", decode_context)
-    make_augmentations(filename, context)
+    make_augmentations(filename, context, qr_filename)
+    context_to_barcode = input("Введите 12-ти значное число для формирования штрих-кода: ")
+    if context_to_barcode == "":
+        context_to_barcode = example_barcode
+    make_bar_code(context_to_barcode)
+    decode_code(cv2.imread(bar_filename_full))
+    make_augmentations(bar_filename_full, decode_code(cv2.imread(bar_filename_full)), bar_filename)
